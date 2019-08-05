7b108e9c9685e92b2baa681aa5d8cb4860f6b425
Denis Bystruev <github@learnSwift.ru>
2019-08-05 20:20:13 +0300

    Initial Commit

---
+++ Hotel Manzana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hotel Manzana
+//
+//  Created by Denis Bystruev on 05/08/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
